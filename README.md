1664f3bec802785eb22e741cd78816614be24daa
johnrlive <john@addlocal.com>
2018-04-27 13:24:19 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## Clone project
+
+```
+git clone git@github.com:johnrlive/tails.git
+cd tails
+```
+
+## Run and promt variables by running:
+
+```
+python tails.py tails ./logs.txt 10
+```
---
+++ tails.py
@@ -0,0 +1,25 @@
+import sys
+
+# set variables manually 
+"""
+name = 'tails'
+file_name = './logs.txt'
+lines = 10
+
+print(name)
+print(file_name)
+print(lines)
+"""
+
+name = sys.argv[1]
+file_name = sys.argv[2]
+lines = sys.argv[3]
+
+# open file, readlines, and assign list to readMe
+readMe = open(file_name).readlines()
+#print(readMe)
+
+# uses the slice notation with the negative integer variable (lines) to get the last elements in the list
+lines = int(lines)
+last_line_slice = readMe[-(lines):]
+print(last_line_slice)
